964ddd0f3295364a2bf3c1e4987f4bc60a07eb3e
2024222760026 <2024222760026@col-8285-li05.cefetes.br>
2026-02-12 14:12:38 -0300

    criando agenda

---
+++ AgendaPlus/src/main/java/com/mycompany/agendaplus/AgendaPlus.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.agendaplus;
+
+/**
+ *
+ * @author 2024222760026
+ */
+public class AgendaPlus {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
